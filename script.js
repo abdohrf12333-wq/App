de4363103f1e378939581658ccf292630bbc1e74
Faro <abdohrf12333@gmail.com>
2026-02-18 22:23:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function goPage(){
+    window.location.href = "page2.html";
+}
+
+function backHome(){
+    window.location.href = "index.html";
+}
+
+function showMessage(){
+    alert("التطبيق شغال 100% من GitHub 🔥");
+}
